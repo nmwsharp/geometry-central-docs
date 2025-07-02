2215765cf964df4239d2c0d42f4db7246ab9d153
Nicholas Sharp <nmwsharp@gmail.com>
2025-07-01 23:36:45 -0400

    more robust build script

--- build_commit_push.sh
+++ build_commit_push.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
+
+# this makes us stop on error
+set -e
+
+# update the underlying lib
 cd geometry-central
 git pull origin master
 cd ../
 git add geometry-central
 git commit -m "pull new version"
+
+# rebuild docs
 python3 -m mkdocs build --config-file geometry-central/docs/mkdocs.yml --site-dir ../../docs
 git add docs/
 git commit -m "rebuild"
+
+# push the result
 git push
